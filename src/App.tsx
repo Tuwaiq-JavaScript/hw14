d38253124ff88abf9e1371f62dd999d185f999f1
A1I21 <87023979+A1I21@users.noreply.github.com>
2022-07-27 00:49:40 +0300

    done

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+
+import './App.css';
+import { Form } from './Components/Form';
+import { Name } from './Components/Name';
+
+function App() {
+  return (
+    <>
+    <div className="App">
+      
+      <Form />
+      {/* <Name name={'ali'}/> */}
+    </div>
+    </>
+  );
+}
+
+export default App;
