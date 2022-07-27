c072a239a1ba2dad232ce291143eab15137ff6f3
OmarSaleh506 <amory2000338@gmail.com>
2022-07-27 03:27:47 +0300

    done

---
+++ hw14-from/src/App.tsx
@@ -0,0 +1,16 @@
+
+import './styles/App.css';
+import From from './components/From';
+import { useEffect, useState } from 'react';
+
+
+
+export default function App() {
+  
+  return (
+    <div className="App">
+    <From />
+    </div>
+  );
+}
+
