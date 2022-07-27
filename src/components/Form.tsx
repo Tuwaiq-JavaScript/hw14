70a7a685d616cf60188f52fb4b629fe526c4724a
meshal1172 <mr.meeshal@hotmail.com>
2022-07-27 12:37:52 +0300

    update

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,5 +1,5 @@
 import { IForm } from '../data';
-import React , {useEffect, useState} from 'react';
+import React , { useState} from 'react';
 
 
 
@@ -8,28 +8,65 @@ interface FormProps {
 }
 
 export function Form({ form }: FormProps) {
-    const [values , setValues] = useState("");
-  const [gender,setGender]=useState("");
+  const [values, setValues] = useState({
+    name: '',
+    email: '',
+    password: '',
+  });
 
+  const [submitted, setSubmitted] = useState(false);
+  const [valid, setValid] = useState(false);
+ 
+  
+  const handleNameInputChange = (e:any) => {
+    e.persist();
+    setValues((values) => ({ ...values, name: e.target.value, }));};
+ 
+    const handleEmailInputChange = (e:any) => {
+    e.persist();
+    setValues((values) => ({ ...values,  email: e.target.value, }));};
+  
+  
+    const handlePasswordInputChange = (e:any) => {
+    e.persist();
+    setValues((values) => ({ ...values,  password: e.target.value, }));};
 
-   
-
+  
+    const handleSubmit = (e:any) => {
+    e.preDefault();
+    if(values.name && values.email && values.password) {
+        setValid(true);
+    }
+    setSubmitted(true);
+    }
+    
 
 
 	return (
 		<div className='form'>
 		<h2>LOGIN</h2>
-        <form>
-            <input type="text" name='username' placeholder='Name' />
-            <input type="text"  name='email' placeholder='Email'/>
-            <input type="password"  name='password' placeholder='Password'/>
+        <form className="register-form" onSubmit={handleSubmit}>
+        {submitted && !valid ?<span id="name-error">"heelo"</span> :null}
+
+            <input type="text" name='username' placeholder='Name'     value={values.name} onChange={ handleNameInputChange}  />
+            {submitted && !values.name ?<span id="name-error">Please enter a  name</span> :null}
+            <input type="text"  name='email' placeholder='Email'     value={values.email} onChange={handleEmailInputChange} />
+            {submitted && !values.email ?<span id="email-error">Please enter a  email</span> :null}
+            <input type="password"  name='password' placeholder='Password'     value={values.password} onChange={handlePasswordInputChange}/>
+            {submitted && !values.password ?<span id="password-error">Please enter a  password</span> :null}
+
             <select>
     <option>Select gender</option>
     <option value="male">Male</option>
     <option value="female">Female</option>
   </select>
-            <button type='submit'>Login</button>
+  <button className='from-filed'
+    type="submit">
+          Register
+        </button>
         </form>
 		</div>
 	);
 }
+
+
